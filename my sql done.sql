df6325552ac4722984340ae129c9063709315bf7
Tomilola <tomilola20f@gmail.com>
2022-08-23 01:52:22 +0200

    my sql done

---
+++ my sql done.sql
@@ -0,0 +1,16 @@
+Use sakila;
+
+select * from actor,film, customer;
+
+
+select title from film;
+
+select "language" from film;
+
+select "company" from store;
+
+select "employe" from store;
+/* employe has two*/
+
+select first_name from staff;
+/* the names are Mike and Jon*/
